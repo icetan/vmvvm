2d15c02eb8d7444f2153dec5db0b2c5e7c16a62e
Christopher Fredén <christopher.freden@kartena.se>
2013-04-10 21:34:45 +0200

    Added CORS and async read template from fs for when running under Node.js

--- test/view.js
+++ test/view.js
@@ -5,53 +5,92 @@ var test = require('tap').test,
 
 test('should create an empty div if no paramaters given', function(t) {
   jsdom.env('<html><body></body></html>', [], function(err, window) {
-    global.window = window;
-    var v = view();
-    t.equal(v.el.tagName, 'DIV');
-    t.ok(v.el instanceof window.HTMLElement);
+    view.window = window;
+    view(null, function(err, v) {
+      t.equal(v.el.tagName, 'DIV');
+      t.ok(v.el instanceof window.HTMLElement);
+    });
   });
   t.plan(2);
 });
 
 test('should create an empty element of type specified as first param', function(t) {
   jsdom.env('<html><body></body></html>', [], function(err, window) {
-    global.window = window;
-    var v = view('span');
-    t.equal(v.el.tagName, 'SPAN');
-    t.ok(v.el instanceof window.HTMLElement);
-    v = view('h1');
-    t.equal(v.el.tagName, 'H1');
-    t.ok(v.el instanceof window.HTMLElement);
+    view.window = window;
+    view({el:'span'}, function(err, v) {
+      t.equal(v.el.tagName, 'SPAN');
+      t.ok(v.el instanceof window.HTMLElement);
+    });
+    view({el:'h1'}, function(err, v) {
+      t.equal(v.el.tagName, 'H1');
+      t.ok(v.el instanceof window.HTMLElement);
+    });
   });
   t.plan(4);
 });
 
 test('should create an empty element of type specified with named params', function(t) {
   jsdom.env('<html><body></body></html>', [], function(err, window) {
-    global.window = window;
-    var div = window.document.createElement('div'),
-        v;
+    view.window = window;
+    var div = window.document.createElement('div');
     div.innerHTML = 'lol rofl';
-    v = view({el: div});
-    t.equal(v.el, div);
-    t.equal(v.el.textContent, 'lol rofl');
-    v = view({el:'span', html:'hej på dig'});
-    t.ok(v.el instanceof window.HTMLElement);
-    t.equal(v.el.tagName, 'SPAN');
-    t.equal(v.el.textContent, 'hej på dig');
+    view({el:div}, function(err, v) {
+      t.equal(v.el, div);
+      t.equal(v.el.textContent, 'lol rofl');
+    });
+    view({el:'span', html:'hej på dig'}, function(err, v) {
+      t.ok(v.el instanceof window.HTMLElement);
+      t.equal(v.el.tagName, 'SPAN');
+      t.equal(v.el.textContent, 'hej på dig');
+    });
   });
   t.plan(5);
 });
 
 test('should extend view model with model param', function(t) {
   jsdom.env('<html><body></body></html>', [], function(err, window) {
-    global.window = window;
-    var v = view('div', '{{lol}}', {lol: 'rofl'});
-    t.equal(v.el.textContent, 'rofl');
-    t.equal(v.model.lol, 'rofl');
-    v = view({el:'div', html:'{{lol}}', model:{lol: 'rofl2'}});
-    t.equal(v.el.textContent, 'rofl2');
-    t.equal(v.model.lol, 'rofl2');
+    view.window = window;
+    view({
+      el: 'div',
+      html: '{{lol}}',
+      model: {lol: 'rofl'}
+    }, function(err, v) {
+      t.equal(v.el.textContent, 'rofl');
+      t.equal(v.model.lol, 'rofl');
+    });
   });
-  t.plan(4);
+  t.plan(2);
+});
+
+test('should add default view model properties to each new view', function(t) {
+  jsdom.env('<html><body></body></html>', [], function(err, window) {
+    view.window = window;
+    view.defaultModel.pi = function(v) {return v.substr(1);};
+    view({
+      el: 'div',
+      html: '{{lol | pi}}',
+      model: {lol: 'rofl'}
+    }, function(err, v) {
+      t.equal(v.model.lol, 'rofl');
+      t.equal(v.el.textContent, 'ofl');
+    });
+  });
+  t.plan(2);
+});
+
+test('should add default view model properties to each new view', function(t) {
+  jsdom.env('<html><body></body></html>', [], function(err, window) {
+    var doc = window.document;
+    view.window = window;
+    view.defaultModel.pi = function(v) {return v.substr(1);};
+    view({
+      el: 'div',
+      path: require.resolve('./data/view-template.html'),
+      model: { a:1, b:2 }
+    }, function(err, v) {
+      t.equal(v.el.querySelector('#a').textContent, '1');
+      t.equal(v.el.querySelector('#b').textContent, '2');
+    });
+  });
+  t.plan(2);
 });
